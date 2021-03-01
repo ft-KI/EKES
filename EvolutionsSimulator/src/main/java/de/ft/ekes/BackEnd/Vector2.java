f2404d18c24a7aa2c775c69e3b89e96f6c0dfe2c
Coder246 <46609883+Coder246@users.noreply.github.com>
2021-03-01 08:05:12 +0100

    Refectored

--- EvolutionsSimulator/src/main/java/de/ft/ekes/BackEnd/Vector2.java
+++ EvolutionsSimulator/src/main/java/de/ft/ekes/BackEnd/Vector2.java
@@ -1,17 +1,20 @@
 package de.ft.ekes.BackEnd;
 
 public class Vector2 {
-    public int x=0;
-    public int y=0;
-    public Vector2(){
+    public int x = 0;
+    public int y = 0;
+
+    public Vector2() {
 
     }
-    public Vector2(int x,int y){
-        this.x=x;
-        this.y=y;
+
+    public Vector2(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    public void set(int x,int y){
-        this.x=x;
-        this.y=y;
+
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
